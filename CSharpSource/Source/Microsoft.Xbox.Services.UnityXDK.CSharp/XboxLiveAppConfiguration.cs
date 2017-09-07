67b55a85306773a8543c15ee1eedf7693bcd3863
John LaFontaine <johlafo@microsoft.com>
2017-09-07 15:20:06 -0700

    adding C# layer for unity XDK

---
+++ CSharpSource/Source/Microsoft.Xbox.Services.UnityXDK.CSharp/XboxLiveAppConfiguration.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// 
+
+namespace Microsoft.Xbox.Services
+{
+    using global::System;
+
+    public partial class XboxLiveAppConfiguration
+    {
+        public static XboxLiveAppConfiguration Load(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
